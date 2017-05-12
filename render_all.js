03a632c920cac31f5de1f336e66ed24c4ab351e0
Matt Biddulph <mb@hackdiary.com>
2017-05-11 21:21:12 -0700

    Sample some neighborhoods and put them in the output json

--- render_all.js
+++ render_all.js
@@ -1,3 +1,4 @@
+const Promise = require('bluebird');
 const get_water_polygons = require('./get_water');
 const AWS = require('aws-sdk');
 const kriging = require('./kriging');
@@ -10,7 +11,7 @@ const util = require('util');
 const Twitter = require('twitter');
 const schedule = require('node-schedule');
 const process = require('process');
-const fs = require('fs');
+const fs = Promise.promisifyAll(require('fs'));
 
 const tweet = png => {
   if (process.env.CONFIG_TWEET !== 'yes') {
@@ -49,6 +50,9 @@ const height = 1334;
 
 const ne = [37.870226, -122.358991];
 const sw = [37.632867, -122.527905];
+
+const readPlaces = () =>
+  fs.readFileAsync('neighborhoods.json').then(f => JSON.parse(f).places);
 
 const s3 = (() => {
   const credentials = new AWS.Credentials(
@@ -102,94 +106,105 @@ const generateMap = () => {
     .colors(120, 'rgb')
     .reverse();
 
-  get_water_polygons({
-    lon_min: sw[1],
-    lat_min: sw[0],
-    lon_max: ne[1],
-    lat_max: ne[0]
-  }).then(json => {
-    const canvas = new Canvas(width, height);
-    const context = canvas.getContext('2d');
-    const proj = d3.geoMercator();
-    proj.fitSize([width, height], json);
-    const path = d3.geoPath().projection(proj).context(context);
+  readPlaces().then(places => {
+    get_water_polygons({
+      lon_min: sw[1],
+      lat_min: sw[0],
+      lon_max: ne[1],
+      lat_max: ne[0]
+    }).then(json => {
+      const canvas = new Canvas(width, height);
+      const context = canvas.getContext('2d');
+      const proj = d3.geoMercator();
+      proj.fitSize([width, height], json);
+      const path = d3.geoPath().projection(proj).context(context);
 
-    token().then(t => getData(t)).then(data => {
-      const points = [];
-      data.forEach(d => {
-        const xy = proj([d[0], d[1]]);
-        points.push([xy[0], xy[1], d[2]]);
-      });
+      token().then(t => getData(t)).then(data => {
+        const points = [];
+        data.forEach(d => {
+          const xy = proj([d[0], d[1]]);
+          points.push([xy[0], xy[1], d[2]]);
+        });
 
-      const x = points.map(d => d[0]);
-      const y = points.map(d => d[1]);
-      const t = points.map(d => d[2]);
-      const model = 'exponential';
-      const sigma2 = 0;
-      const alpha = 100;
-      const variogram = kriging.train(t, x, y, model, sigma2, alpha);
+        const x = points.map(d => d[0]);
+        const y = points.map(d => d[1]);
+        const t = points.map(d => d[2]);
+        const model = 'exponential';
+        const sigma2 = 0;
+        const alpha = 100;
+        const variogram = kriging.train(t, x, y, model, sigma2, alpha);
 
-      const output = {
-        points: []
-      };
-      const canvasData = context.getImageData(0, 0, width, height);
-      for (let ypos = 0; ypos < height; ypos += 1) {
-        const line = [];
-        for (let xpos = 0; xpos < height; xpos += 1) {
-          const val = kriging.predict(xpos, ypos, variogram);
-          if (xpos % 10 === 0 && ypos % 10 === 0) {
-            line.push({
-              ll: proj.invert([xpos, ypos]).map(l => l.toFixed(4)),
-              t: val.toFixed(1)
+        const output = {
+          points: [],
+          places: places.map(p => {
+            const xy = proj([p.latlon[1], p.latlon[0]]);
+            return Object.assign({}, p, {
+              temp_in_c: kriging.predict(xy[0], xy[1], variogram).toFixed(1),
+              x: xy[0].toFixed(0),
+              y: xy[1].toFixed(1)
             });
+          })
+        };
+        const canvasData = context.getImageData(0, 0, width, height);
+        for (let ypos = 0; ypos < height; ypos += 1) {
+          const line = [];
+          for (let xpos = 0; xpos < height; xpos += 1) {
+            const val = kriging.predict(xpos, ypos, variogram);
+            if (xpos % 10 === 0 && ypos % 10 === 0) {
+              line.push({
+                ll: proj.invert([xpos, ypos]).map(l => l.toFixed(4)),
+                t: val.toFixed(1)
+              });
+            }
+            const index = (xpos + ypos * width) * 4;
+            const color = colors[Math.floor(2 * (val + 20))];
+            canvasData.data[index] = color[0];
+            canvasData.data[index + 1] = color[1];
+            canvasData.data[index + 2] = color[2];
+            canvasData.data[index + 3] = 255;
           }
-          const index = (xpos + ypos * width) * 4;
-          const color = colors[Math.floor(2 * (val + 20))];
-          canvasData.data[index] = color[0];
-          canvasData.data[index + 1] = color[1];
-          canvasData.data[index + 2] = color[2];
-          canvasData.data[index + 3] = 255;
+          if (line.length > 0) {
+            output.points.push(line);
+          }
         }
-        if (line.length > 0) {
-          output.points.push(line);
+        context.putImageData(canvasData, 0, 0);
+
+        context.fillStyle = 'lightblue';
+        context.beginPath();
+        path(json);
+        context.fill();
+
+        tweet(canvas.toBuffer());
+        const filenameBase = `temps-${new Date().toISOString()}`;
+        if (process.env.CONFIG_S3_UPLOAD === 'yes') {
+          const put1 = s3
+            .putObject({
+              Bucket: 'tempmap',
+              Key: `${filenameBase}.png`,
+              Body: canvas.toBuffer(),
+              ContentType: 'image/png',
+              ACL: 'public-read'
+            })
+            .promise();
+          const put2 = s3
+            .putObject({
+              Bucket: 'tempmap',
+              Key: `${filenameBase}.json`,
+              Body: JSON.stringify(output, null, 2),
+              ContentType: 'application/json',
+              ACL: 'public-read'
+            })
+            .promise();
+          Promise.all([put1, put2])
+            .then(() => console.log('Uploaded'))
+            .catch(err => console.log('Upload error', err));
+        } else {
+          Promise.all([
+            fs.writeFileAsync('out.png', canvas.toBuffer()),
+            fs.writeFileAsync('out.json', JSON.stringify(output, null, 2))
+          ]).then(() => console.log('Written files'));
         }
-      }
-      context.putImageData(canvasData, 0, 0);
-
-      context.fillStyle = 'lightblue';
-      context.beginPath();
-      path(json);
-      context.fill();
-
-      tweet(canvas.toBuffer());
-      const filenameBase = `temps-${new Date().toISOString()}`;
-      if (process.env.CONFIG_S3_UPLOAD === 'yes') {
-        const put1 = s3
-          .putObject({
-            Bucket: 'tempmap',
-            Key: `${filenameBase}.png`,
-            Body: canvas.toBuffer(),
-            ContentType: 'image/png',
-            ACL: 'public-read'
-          })
-          .promise();
-        const put2 = s3
-          .putObject({
-            Bucket: 'tempmap',
-            Key: `${filenameBase}.json`,
-            Body: JSON.stringify(output, null, 2),
-            ContentType: 'application/json',
-            ACL: 'public-read'
-          })
-          .promise();
-        Promise.all([put1, put2])
-          .then(() => console.log('Uploaded'))
-          .catch(err => console.log('Upload error', err));
-      } else {
-        fs.writeFileSync('out.png', canvas.toBuffer());
-        fs.writeFileSync('out.json', JSON.stringify(output, null, 2));
-        console.log('Written files');
-      }
+      });
     });
   });
 };
